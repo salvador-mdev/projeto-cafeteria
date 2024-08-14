3b582da7a5386073384922bf61812e853a01c698
Salvador Montero <salvadormdev@gmail.com>
2024-08-14 07:23:20 -0400

    agregando animaciones al proyecto con scrollreveal

---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+window.sr = ScrollReveal();
+  sr.reveal('.header ul .li1', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-50px'
+  });
+
+  sr.reveal('.header ul .li2', {
+    duration: 2750,
+    origin: 'bottom',
+    distance: '-75px'
+  });
+
+  sr.reveal('.header ul .li3', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.header ul .li4', {
+    duration: 3250,
+    origin: 'bottom',
+    distance: '-125px'
+  });
+
+  sr.reveal('.header ul .li5', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-150px'
+  });
+
+  sr.reveal('.hero .texto', {
+    duration: 2500,
+    origin: 'rigth',
+    distance: '-300px'
+  });
+
+  sr.reveal('.about', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.gallery', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.testimonials .testemunhas', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
