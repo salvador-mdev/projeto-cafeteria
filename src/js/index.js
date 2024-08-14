7c503d771aeb5727adc747242b5d29f41821e99c
Salvador Montero <salvadormdev@gmail.com>
2024-08-14 09:52:38 -0400

    modificacion animacion elementos del menu desactivado, no compatible con el responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,33 @@
 window.sr = ScrollReveal();
-  sr.reveal('.header ul .li1', {
-    duration: 2500,
-    origin: 'bottom',
-    distance: '-50px'
-  });
+  // sr.reveal('.header ul .li1', {
+  //   duration: 2500,
+  //   origin: 'bottom',
+  //   distance: '-50px'
+  // });
 
-  sr.reveal('.header ul .li2', {
-    duration: 2750,
-    origin: 'bottom',
-    distance: '-75px'
-  });
+  // sr.reveal('.header ul .li2', {
+  //   duration: 2750,
+  //   origin: 'bottom',
+  //   distance: '-75px'
+  // });
 
-  sr.reveal('.header ul .li3', {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '-100px'
-  });
+  // sr.reveal('.header ul .li3', {
+  //   duration: 3000,
+  //   origin: 'bottom',
+  //   distance: '-100px'
+  // });
 
-  sr.reveal('.header ul .li4', {
-    duration: 3250,
-    origin: 'bottom',
-    distance: '-125px'
-  });
+  // sr.reveal('.header ul .li4', {
+  //   duration: 3250,
+  //   origin: 'bottom',
+  //   distance: '-125px'
+  // });
 
-  sr.reveal('.header ul .li5', {
-    duration: 3500,
-    origin: 'bottom',
-    distance: '-150px'
-  });
+  // sr.reveal('.header ul .li5', {
+  //   duration: 3500,
+  //   origin: 'bottom',
+  //   distance: '-150px'
+  // });
 
   sr.reveal('.hero .texto', {
     duration: 2500,
@@ -53,7 +53,19 @@ window.sr = ScrollReveal();
     distance: '-300px'
   });
 
-  sr.reveal('.testimonials .testemunhas', {
+  sr.reveal('.testemunho1', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.testemunho2', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.testemunho3', {
     duration: 3500,
     origin: 'top',
     distance: '-300px'
